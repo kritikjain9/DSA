77be18fe7422689cc5c52b001969ce09aa964006
kritik jain <kritikjain9@gmail.com>
2021-12-26 21:32:27 +0530

    some more codes

--- src/binarySearch/LeetCodeBinarySearch.java
+++ src/binarySearch/LeetCodeBinarySearch.java
@@ -55,11 +55,6 @@ public class LeetCodeBinarySearch {
 	        }
 			return -1;
 	}
-	
-		public int peakIndexInMountainArray(int[] arr) {
-			
-	        return 0;
-	    }
 		
 		public static int ans(int[] arr, int tar) {
 	        // first find the range
@@ -89,11 +84,74 @@ public class LeetCodeBinarySearch {
 	        }
 	        return - 1;
 	    }
+		
+		public static boolean binarySearch(int[] arr, int tar) {
+			int st = 0, ending = arr.length - 1;
+			while(st <= ending){
+	            int mid = st + (ending - st) / 2;
+	            if(tar > arr[mid]){
+	                st = mid + 1;
+	            }else if(tar < arr[mid]){
+	            	ending = mid - 1;
+	            }else if(tar == arr[mid]){	
+	                return true;
+	            }
+	        }
+	        return false;
+		}
+		
+		public static int findInMountainArray(int[] arr, int tar) {
+			boolean res = binarySearch(arr, tar);
+			if(!res)return -1;
+			
+			int st = 0, ending = arr.length - 1;
+			while(st <= ending){
+	            int mid = st + (ending - st) / 2;
+	            if(tar > arr[mid]){
+	                st = mid + 1;
+	            }else if(tar < arr[mid]){
+	            	ending = mid - 1;
+	            }else if(tar == arr[mid]){	
+	                ending = mid - 1;
+	            }
+	        }
+	        return st;
+		}
+		
+		public static int pivotInDuplicateRotated(int[] arr) {
+			int left = 0, right = arr.length - 1;
+			while(left <= right) {
+				if(arr[left] == arr[right]) {
+					left += 1; right += 1;
+				}
+				
+				int mid = left + (right - left)/2;
+				if(mid < right && arr[mid] > arr[mid+1]) {
+					return mid;
+				}
+				else if(mid > left && arr[mid] < arr[mid - 1]) {
+					return mid - 1;
+				}
+				
+				else if(arr[left] > arr[mid]) {
+					//left part will have the pivot
+					right = mid - 1;
+				}
+				
+				else {
+					left = mid + 1;
+				}
+				
+			}
+			return -1;
+		}
 	
 	public static void solver() {
-		int[] nums = {5, 7, 8, 10, 12, 14, 16, 19, 20, 21, 22};
+		int[] arr = {4,4,5,5,6,9,1,2,2,3,4};
+		System.out.println(pivotInDuplicateRotated(arr));
+//		int[] nums = {0,1,2,4,2,1};
 //		System.out.println(binSearchInfinite(nums, 22));
-		System.out.println(ans(nums, 22));
+//		System.out.println(findInMountainArray(nums, 3));
 //		int[] res = searchRange(nums, 8);
 //		System.out.println(Arrays.toString(res));
 		
