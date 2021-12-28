c8d46c58366a56b13a06b682304c7dbfc4b173f8
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2021-12-28 22:29:49 +0530

    Time: 0 ms (100.00%), Space: 35.3 MB (98.27%) - LeetHub

--- valid-perfect-square/valid-perfect-square.java
+++ valid-perfect-square/valid-perfect-square.java
@@ -1,11 +1,27 @@
 class Solution {
-    public boolean isPerfectSquare(int n) {
-         
-        // int res = Math.sqrt(n);
-        // if(Math.sqrt(n) == (int)res){
-            if(Math.sqrt(n) % 1 == 0){
+    public boolean isPerfectSquare(int num) {
+        if(num < 2)
             return true;
+        
+        //if 'x' is the sq. root of num, then x stricly lies in the range  1 < x < num / 2
+        //so, we will take these two points as our start and end, and will then assume our result using the mid element
+        
+        long start = 2, end = num / 2;
+        while(start <= end){
+            long x = start + (end - start) / 2;
+            long square = x * x;
+            
+            //logic
+            //If square == num, then the perfect square is right here, return True
+            if(square == num)
+                return true;
+            else if(square > num)       //decreasing the right boundary
+                end = x - 1;
+            else
+                start = x + 1;      //increasing the left boundary
         }
+        
+        //if the while loop is over, that means num is not a perfect square
         return false;
     }
 }
