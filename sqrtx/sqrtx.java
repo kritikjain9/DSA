1a9218a2d8934dc7080003a8f5a6f0a7b47366d2
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2021-12-28 23:04:38 +0530

    Time: 1 ms (99.98%), Space: 35.8 MB (88.70%) - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -1,5 +1,24 @@
 class Solution {
-    public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+    public int mySqrt(int num) {
+        if(num < 2)
+            return num;
+        
+        if(num == 2 || num == 3)
+            return 1;
+        
+        //1 < square_root < num/2;
+        int st = 0, end = num/2;
+        while(st <= end){
+            int sqRoot = st + (end - st)/2;
+            long res = (long) sqRoot * sqRoot;
+            
+            if(res == num)
+                return sqRoot;
+            else if(res > num)
+                end = sqRoot - 1;
+            else
+                st = sqRoot + 1;
+        }
+        return end;
     }
 }
