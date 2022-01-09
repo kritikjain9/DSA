47a369d4bd84530c44d4d377ac263a58cfcc29eb
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2022-01-09 17:02:40 +0530

    Time: 4 ms (41.61%), Space: 52 MB (14.05%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.java
+++ 645-set-mismatch/645-set-mismatch.java
@@ -1,61 +1,28 @@
 class Solution {
-    public int[] findErrorNums(int[] nums) {
-        int[] res = new int[2];
-        
-        res[0] = findDuplicateNumber(nums);
-        res[1] = missingNumber(nums);
-        
-        return res;
-    }
-    
-    public int findDuplicateNumber(int[] nums) {
+    public int[] findErrorNums(int[] arr) {
         int idx = 0;
-        
-        while(idx < nums.length){
-            int correctIdx = nums[idx] - 1;
+        while(idx < arr.length){
+            int correctIdx = arr[idx] - 1;
             
-            if(nums[idx] != idx+1){
-                if(nums[idx] == nums[correctIdx]){
-                    return nums[idx];
-                }else{
-                    int temp = nums[idx];
-                    nums[idx] = nums[correctIdx];
-                    nums[correctIdx] = temp;
-                }
+            if(arr[idx] == arr[correctIdx]){
+                idx++;
             }else{
-                idx++;
+                swap(arr, idx, correctIdx);
             }
-            
-        }
-        return -1;
-    }
-    
-    public int missingNumber(int[] nums) {
-        int idx = 0;
-
-        while(idx < nums.length){
-            if(nums[idx] == nums.length){
-                idx++;
-                continue;
-            }
-            
-            int correctIdx = nums[idx] - 1;
-            if(nums[idx] == nums[correctIdx]){
-                idx++;
-            }
-            else{
-                int temp = nums[idx];
-                nums[idx] = nums[correctIdx];
-                nums[correctIdx] = temp;
-            } 
         }
         
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != i+1){
-                return i+1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != i+1){
+                return new int[]{arr[i], (i+1)};
             }
         }
-        return nums.length;
+        return new int[]{-1, -1};
+    }
+    
+    public void swap(int[] arr, int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
 }
