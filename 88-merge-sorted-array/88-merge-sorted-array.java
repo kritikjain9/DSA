43c3ca17d17895f5efe0cdd4ab867a01f750ac50
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2022-01-12 18:04:22 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (84.94%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,8 +1,31 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = 0; i < n; i++) {
-            nums1[i + m] = nums2[i];
+        int[] n1Copy = new int[m];
+        for(int i = 0; i < m; i++){
+            n1Copy[i] = nums1[i];
         }
-        Arrays.sort(nums1);
+        
+        int i = 0, j = 0, k = 0;
+        while(i < m && j < n){
+            if(n1Copy[i] <= nums2[j]){
+                nums1[k] = n1Copy[i];
+                i++; k++;
+            }
+            else{
+                nums1[k] = nums2[j];
+                j++; k++;
+            }
+        }
+        
+        for(int rem = i;  rem < m; rem++){
+            nums1[k] = n1Copy[rem];
+            k++;
+        }
+        
+        for(int rem = j;  rem < n; rem++){
+            nums1[k] = nums2[rem];
+            k++;
+        }
+        
     }
 }
