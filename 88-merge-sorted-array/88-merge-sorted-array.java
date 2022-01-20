579d126157e66779ede8696a67f623c8c57255cc
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2022-01-20 19:50:05 +0530

    Time: 0 ms (100.00%), Space: 39 MB (76.87%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,31 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] n1Copy = new int[m];
-        for(int i = 0; i < m; i++){
-            n1Copy[i] = nums1[i];
-        }
-        
-        int i = 0, j = 0, k = 0;
-        while(i < m && j < n){
-            if(n1Copy[i] <= nums2[j]){
-                nums1[k] = n1Copy[i];
-                i++; k++;
-            }
-            else{
-                nums1[k] = nums2[j];
-                j++; k++;
-            }
-        }
-        
-        for(int rem = i;  rem < m; rem++){
-            nums1[k] = n1Copy[rem];
-            k++;
-        }
-        
-        for(int rem = j;  rem < n; rem++){
-            nums1[k] = nums2[rem];
-            k++;
-        }
-        
+    int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
+    while (tail1 >= 0 && tail2 >= 0) {
+        nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? 
+                             nums1[tail1--] : nums2[tail2--];
     }
+
+    while (tail2 >= 0) { //only need to combine with remaining nums2
+        nums1[finished--] = nums2[tail2--];
+    }
+}
 }
