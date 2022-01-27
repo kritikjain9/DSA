b9e0d6b3e43163ea6506b022255cd1595fe412f0
kritik jain <kritikjain9@gmail.com>
2022-01-27 21:48:36 +0530

    added some more codes

--- src/leetCode/StringsSubsetsSubsequenceRecursion.java
+++ src/leetCode/StringsSubsetsSubsequenceRecursion.java
@@ -1,4 +1,5 @@
 package leetCode;
+import java.util.*;
 
 public class StringsSubsetsSubsequenceRecursion {
 
@@ -72,18 +73,68 @@ public class StringsSubsetsSubsequenceRecursion {
 			System.out.println(res);
 			return;
 		}
-		
+
 		char ch = str.charAt(0);
 		printSubsets(str.substring(1), res+ch);
 		printSubsets(str.substring(1), res);
 	}
-
-	public static void solver() {
-		printSubsets("abc", "");
-//		System.out.println(removeUnwantedKeepWanted("A_GOOD_appleapp_appapple_STRING", "apple", "app"));
-//		System.out.println(removeUnwanted("kritikjainisnotabeast", "kritikjain"));
-//		removeAs(str, 0, res);
-//		System.out.println(_removeAs("baccaaaaaaad", 0));
+	
+	public static ArrayList<String> returnSubsets(String str) {
+		if(str.isEmpty()) {
+			ArrayList<String> res = new ArrayList<>();
+			res.add("");
+			return res;
+		}
+		
+		char ch = str.charAt(0);
+		ArrayList<String> rres = returnSubsets(str.substring(1));
+		ArrayList<String> myRes = new ArrayList<>();
+		for(String s : rres) {
+			myRes.add("" + s);
+		}
+		for(String s : rres) {
+			myRes.add(ch + s);
+		}
+		return myRes;
 	}
+	
+	public static void printAsciiSubsets(String str, String res) {
+		if(str.isEmpty()) {
+			System.out.println(res);
+			return;
+		}
+		
+		char ch = str.charAt(0);
+		printAsciiSubsets(str.substring(1), res);
+		printAsciiSubsets(str.substring(1), res+ch);
+		printAsciiSubsets(str.substring(1), res+(int)ch);
+	}
+	
+	
+	public static ArrayList<String> returnAsciiSubsets(String str, String res){
+		if(str.isEmpty()) {
+			ArrayList<String> bc = new ArrayList<>();
+			bc.add(res);
+			return bc;
+		}
+		
+		char ch = str.charAt(0);
+		
+		ArrayList<String> left = returnAsciiSubsets(str.substring(1), res);
+		ArrayList<String> right = returnAsciiSubsets(str.substring(1), res + ch);
+		ArrayList<String> mid = returnAsciiSubsets(str.substring(1), res + (int)ch);
+		
+		left.addAll(right);
+		left.addAll(mid);
+		
+		return left;
+	}
+	
+
+		public static void solver() {
+	//	System.out.println(printSubsets("abc"));
+	//	printAsciiSubsets("ab ","");
+			System.out.println(returnAsciiSubsets("ab", ""));
+		}
 	
 }
