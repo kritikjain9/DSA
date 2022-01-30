f6bda16538f12528bfdce8150a7df7c22e03ea3c
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2022-01-30 15:04:06 +0530

    Time: 2 ms (7.75%), Space: 41.6 MB (6.95%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,24 +1,62 @@
 class Solution {
-//     public int search(int[] nums, int target) {
-//         int st = 0, end = nums.length - 1;
+    public int search(int[] arr, int target) {
+        if(arr.length == 1){
+            if(arr[0] == target)
+                return 0;
+            else
+                return -1;
+        }
+        
+        int pivot = findPivot(arr);
+        System.out.println(pivot);
+        
+        int leftSearch = binarySearch(arr, target, 0, pivot);
+        if(leftSearch != -1)
+            return leftSearch;
+        
+        int rightSearch = binarySearch(arr, target, pivot + 1, arr.length - 1);
+        if(rightSearch != -1)
+            return rightSearch;
+        
+        return -1;
+    }
     
-//         while(st <= end){
-//             int mid = st + (end - st)/2;
-            
-//             if(arr[mid] == tar){
-//                 return mid;
-//             }
-//             else if()
-            
-//         }
-//     }
-    public int search(int[] nums, int tar){
+    public int binarySearch(int[] arr, int tar, int st, int end){
+    
         int res = -1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == tar){
-                res = i;
-            }
+        while(st <= end){
+            int mid = st + (end - st) / 2;
+            
+            if(arr[mid] == tar)
+                return mid;
+            else if(arr[mid] < tar)
+                st = mid + 1;
+            else
+                end = mid - 1;
         }
         return res;
     }
+    
+    public int findPivot(int[] arr){
+        if(arr[0] < arr[arr.length - 1])
+            return arr.length - 1;
+        
+        int res = -1;
+        int st = 0, end = arr.length - 1;
+        while(st <= end){
+            int mid = st + (end - st) / 2;
+            if(mid < end && arr[mid] > arr[mid + 1])
+                res = mid;
+            else if(mid > st && arr[mid] < arr[mid - 1])
+                res = mid - 1;
+                
+            //checking where to search
+            if(arr[st] >= arr[mid])
+                end = mid - 1;
+            else
+                st = mid + 1;
+        }
+        return res;
+    }
+    
 }
