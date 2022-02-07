70746547e70ab0f212bc98c933f611b11a06d35c
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2022-02-08 03:09:50 +0530

    Time: 0 ms (100.00%), Space: 45.5 MB (5.59%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.java
+++ 328-odd-even-linked-list/328-odd-even-linked-list.java
@@ -10,59 +10,30 @@
  */
 class Solution {
     
-    public static ListNode oddEvenList(ListNode head) {
+    
+    public ListNode oddEvenList(ListNode head) {
         if(head == null || head.next == null)
             return head;
         
-        ListNode odd = new ListNode(-1);
-        ListNode even = new ListNode(-1);
-        ListNode oddTail = odd, evenTail = even;
+        ListNode p1, p2;
+        p1 = head;
+        p2 = head.next;
         
-        ListNode curr = head;
-        boolean flag = true;
-        while(curr != null){
-            if(flag){
-                oddTail.next = curr;
-                oddTail = curr;  
-            }else{
-                evenTail.next = curr;
-                evenTail = curr;
-            }
-            flag = !flag;
-            curr = curr.next;
+        ListNode newNext = p2;
+        
+        //processing
+        while(p2 != null && p2.next != null){
+            p1.next = p1.next.next;
+            p1 = p1.next;
+            
+            p2.next = p2.next.next;
+            p2 = p2.next;
         }
+        p1.next = null;
+        // p2.next = null;
         
-        evenTail.next = null;
-        oddTail.next = even.next;
-        return odd.next;
+        p1.next = newNext;
+        return head;
+        
     }
-    
-//     public ListNode oddEvenList(ListNode head) {
-//         if(head == null || head.next == null)
-//             return head;
-        
-//         ListNode p1, p2;
-//         p1 = head;
-//         p2 = head.next;
-        
-//         ListNode newHead = p1;
-//         ListNode newNext = p2;
-        
-//         //processing
-//         while(p1.next != null && p1.next.next != null){
-//             p1.next = p1.next.next;
-//             p1 = p1.next;
-//         }
-//         p1.next = null;
-        
-//         while(p2.next != null && p2.next.next != null){
-//             p2.next = p2.next.next;
-//             p2 = p2.next;
-//         }
-//         p2.next = null;
-        
-//         p1.next = newNext;
-//         return newHead;
-        
-//     }
 }
