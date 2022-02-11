f3115a1b7ed0b1c6383a6dc906310f17c04f13c2
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2022-02-12 03:11:25 +0530

    Time: 2 ms (69.18%), Space: 55.2 MB (5.04%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] res = new int[nums.length];
+        
+        int idx = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0)
+                res[idx++] = nums[i];
+        }
+        
+        for(int i = 0; i < res.length; i++){
+            nums[i] = res[i];
+        }
+    }
+}
