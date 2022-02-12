dcfdd674df3c0f02a8aa8616ed5cfdff441fc49e
Kritik Jain <55712009+thebeastkritik9@users.noreply.github.com>
2022-02-12 22:49:01 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (5.34%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public int size(ListNode head){
+        int count = 0;
+        for(ListNode temp = head; temp != null; temp = temp.next)
+            count++;
+        
+        return count;
+    }
+    
+    public ListNode oh = null, ot = null, th = null, tt = null;
+    
+    public void addFirst(ListNode node){
+        if(th == null){
+            th = node;
+            tt = node;
+        }
+        else{
+            node.next = th;
+            th = node;
+        }
+    }
+    
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null)return head;
+        int k = 2;
+        
+        int count = size(head);
+        ListNode curr = head; 
+        
+        //outer while condition
+        while(curr != null && count >= k){
+            
+            int revCount = k;
+            while(revCount > 0){
+                ListNode fwd = curr.next;
+                curr.next = null;
+                addFirst(curr);
+                curr = fwd;
+                revCount--;
+            }
+            
+            count -= k;
+            
+            if(oh == null){
+                oh = th;
+                ot = tt;
+            }
+            else{
+                ot.next = th;
+                ot = tt;
+            }
+            
+            th = null;
+            tt = null;
+            
+        }
+        
+        if(curr != null){
+            ot.next = curr;
+        }
+        return oh;
+    }
+    
+}
