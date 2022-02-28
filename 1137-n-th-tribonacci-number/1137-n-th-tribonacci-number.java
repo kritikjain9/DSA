2506558906c23fd99957aa3198850b4da639b5b3
Kritik Jain <55712009+kritikjain9@users.noreply.github.com>
2022-03-01 02:32:54 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (12.66%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,16 +1,22 @@
 class Solution {
     public int tribonacci(int n) {
+        int[] dp = new int[n + 1];
+        int res = fibo(n, dp);
+        return res;
+    }
+    
+    public int fibo(int n, int[] dp){
         if(n < 2)return n;
         if(n == 2)return 1;
         
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        if(dp[n] != 0)return dp[n];
         
-        for(int i = 3; i <= n; i++)
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        
-        return dp[n]; 
+        int fnm1 = fibo(n-1, dp);
+        int fnm2 = fibo(n-2, dp);
+        int fnm3 = fibo(n-3, dp);
+
+        dp[n] = fnm1 + fnm2 + fnm3;    
+        return dp[n];
     }
+    
 }
