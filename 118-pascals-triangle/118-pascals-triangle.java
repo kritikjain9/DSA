aab37d01c05c01664aa84fa3da09bca6a0a2984d
Kritik Jain <55712009+kritikjain9@users.noreply.github.com>
2022-03-16 12:38:01 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (7.73%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -6,32 +6,15 @@ class Solution {
             dp[i][0] = 1;
         }
         
-        // for(int i = 1; i < numRows; i++){
-        //     for(int j = 1; j < numRows; j++){
-        //         if(i == j){
-        //             dp[i][j] = 1;
-        //         }
-        //     }
-        // }
-        
         for(int i = 1; i < numRows; i++){
             for(int j = 1; j < numRows; j++){
                 dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
             }
         }
         
-        // for(int i = 0; i < numRows; i++){
-        //     for(int j = 0; j < numRows; j++){
-        //         System.out.print(dp[i][j] + " ");
-        //     }            
-        //     System.out.println();
-        // }
-        
-        // List<List<Integer>> master = new ArrayList<ArrayList<Integer>>();
         List<List<Integer>> master = new ArrayList();
 
         for(int i = 0; i < numRows; i++){
-            // ArrayList<Integer> rowList = new ArrayList<>();
             List<Integer> rowList = new ArrayList<Integer>();
             for(int j = 0; j < numRows; j++){
                 if(dp[i][j] != 0){
@@ -40,6 +23,7 @@ class Solution {
             }
             master.add(rowList);
         }
+        
         return master;
     }
 }
