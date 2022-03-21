d425d44aed80f506235780205131b80dac1e93fc
Kritik Jain <55712009+kritikjain9@users.noreply.github.com>
2022-03-21 12:47:21 +0530

    Time: 6 ms (89.06%), Space: 42.1 MB (92.27%) - LeetHub

---
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int val : deck){
+            hm.put(val, hm.getOrDefault(val, 0) + 1);
+        }
+        
+        int res = 0;
+        for(int i : hm.values()){
+            res = gcd(i, res);
+        }
+        
+        return res >= 2;
+    }
+    
+    public int gcd(int a, int b) {
+        return b > 0 ? gcd(b, a % b) : a;
+    }
+    
+    // public int gcd(int a, int b){
+    //     while(){
+    //         int rem = a % b;
+    //         a = b;
+    //         b = rem;
+    //     }
+    //     return a;
+    // }
+    
+}
