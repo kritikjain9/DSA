c5493d2de929e7e9b2215067eb370363dac473b3
Kritik Jain <55712009+kritikjain9@users.noreply.github.com>
2022-03-22 14:10:09 +0530

    Time: 4 ms (25.12%), Space: 42.1 MB (46.35%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,33 +1,47 @@
 class Solution {
-    public boolean isValid(String exp) {
-        
-       Stack <Character> st = new Stack<>();
-        for(int i = 0; i < exp.length(); i++){
-            char ch = exp.charAt(i);
-
-            if(ch == '(' || ch == '{' || ch == '['){        
-                st.push(ch);
-            }
-            else if(ch == ')' || ch == '}' || ch == ']'){  
-                if(st.size() == 0)return false;             
-                
-                else if(ch == ')' && st.peek() != '('){     //bracket mismatch
-                    return false;
-                }
-                else if(ch == '}' && st.peek() != '{'){
-                    return false;
-                }
-                else if(ch == ']' && st.peek() != '['){
-                    return false;
-                }
-                st.pop();
-            }
-        }
-
-        if(st.size() != 0){                                 // no. of opening B > no closing B 
+    public boolean isValid(String str) {
+        if(str.length() % 2 != 0){
             return false;
         }
-
+        
+        Stack<Character> st = new Stack<>() ;
+        
+        for(int i = 0 ; i<str.length() ; i++){
+            char ch = str.charAt(i) ;
+            
+            if(ch=='(' ||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.size() == 0){
+                    return false;
+                }
+                //check for particular closing bracket
+                if(ch == ')'){
+                    if(st.size() > 0 && st.peek() != '('){
+                        return false;
+                    }
+                }
+                
+                else if(ch == ']'){
+                    if(st.size() > 0 && st.peek() != '['){
+                        return false;
+                    }
+                }
+                else if(ch == '}'){
+                    if(st.size() > 0 && st.peek() != '{'){
+                        return false;
+                    }
+                }
+                
+                    st.pop();
+            }
+        }
+        
+        if(st.size() != 0){
+            return false;
+        }
+        
         return true;
     }
 }
