8c4d60df4e9ca0c40369147623bbef86f948d143
Kritik Jain <55712009+kritikjain9@users.noreply.github.com>
2022-06-01 16:29:09 +0530

    Time: 1 ms (74.17%), Space: 43.7 MB (39.54%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -0,0 +1,99 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    final int CAMERA = 1;
+    final int MONITORED = 2;
+    final int NOT_MONITORED = 3;
+
+    public int cCount = 0;
+    
+    public int minCameraCover(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+        int count = helper(root);
+        if(count == NOT_MONITORED){
+            cCount++;
+        }
+        
+        return cCount;
+    }
+    
+    public int helper(TreeNode root){
+        if(root == null){
+            return MONITORED;
+        }
+        
+        int lRes = helper(root.left);
+        int rRes = helper(root.right);
+
+        if(lRes == NOT_MONITORED || rRes == NOT_MONITORED){
+            cCount++;
+            return CAMERA;
+        }else if(lRes == CAMERA || rRes == CAMERA){
+            return MONITORED;
+        }else{
+            return NOT_MONITORED;
+        }
+    }
+    
+}
+
+
+// class Solution {
+//     int count = 0;
+//     final int CAMERA = 1;
+//     final int MONITORED = 2;
+//     final int NOT_MONITORED = 3;
+
+//     public int minCameraCover(TreeNode root) {
+//      int val = helper(root);
+
+//      if(val == NOT_MONITORED){
+//          count++;
+//      }
+     
+//      return count;   
+//     }
+
+//     public int helper(TreeNode node){
+//         if(node == null){
+//             return MONITORED;
+//         }
+
+//         int left = helper(node.left);
+//         int right = helper(node.right);
+
+//         if(left == NOT_MONITORED || right == NOT_MONITORED){
+//             count++;
+//             return CAMERA;
+//         } else if(left == CAMERA || right == CAMERA){
+//             return MONITORED;
+//         } else {
+//             return NOT_MONITORED;
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
