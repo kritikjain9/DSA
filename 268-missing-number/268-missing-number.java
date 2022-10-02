944b92e38c994e64326831f0cde1cc4ecb91c8b7
Kritik Jain <kritikjain9@gmail.com>
2022-10-02 23:34:57 +0530

    Time: 11 ms (11.63%), Space: 51.3 MB (31.75%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,24 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int idx = 0;
-
-        while(idx < nums.length){
-            if(nums[idx] == nums.length){
-                idx++;
-                continue;
-            }
-            
-            int correctIdx = nums[idx];
-            if(nums[idx] == nums[correctIdx]){
-                idx++;
-            }
-            else{
-                int temp = nums[idx];
-                nums[idx] = nums[correctIdx];
-                nums[correctIdx] = temp;
-            } 
-        }
-        
+        int idx = -1;
+        Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++){
             if(nums[i] != i){
                 return i;
