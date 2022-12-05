6eed689415c25f98d1bc1aac23067f2e60f23416
Kritik Jain <kritikjain9@gmail.com>
2022-12-05 23:53:48 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (88.02%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,12 +1,17 @@
 class Solution {
     public boolean check(int[] arr) {
-        int irregularities = 0;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] > arr[(i+1) % arr.length])
-                irregularities += 1;
+        if(arr.length == 1){
+            return true;
         }
         
-        return irregularities > 1  ? false : true;
+        int k = 0;
         
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > arr[(i+1) % arr.length]){
+                k++;
+            }
+        }
+        
+        return k > 1 ? false : true;
     }
 }
