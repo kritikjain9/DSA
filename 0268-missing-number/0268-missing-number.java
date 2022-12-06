be6860ed5e717ce0d7973dfeaad65788594ccf44
Kritik Jain <kritikjain9@gmail.com>
2022-12-06 22:43:20 +0530

    Time: 8 ms (24.80%), Space: 50.8 MB (64.35%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        
+        if(nums[0] != 0)
+            return 0;
+        
+        if(nums[nums.length - 1] != nums.length)
+            return nums.length;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
