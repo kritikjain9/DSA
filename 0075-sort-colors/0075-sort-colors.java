888c8d3a72c2014ef66a9aedcfdc924bcedd8592
Kritik Jain <kritikjain9@gmail.com>
2022-12-17 23:40:38 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (97.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int i = 0, j = 0, k = nums.length - 1;
+        
+        while(i <= k){
+            
+            if(nums[i] == 0){
+                swap(nums, i, j);
+                i++;
+                j++;
+            }
+            else if(nums[i] == 2){
+                swap(nums, i, k);
+                k--;
+            }
+            else{
+                i++;
+            }
+        }
     }
+    
+    private void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
 }
