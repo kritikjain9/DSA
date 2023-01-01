6066cc815942c9ed953405918611fa82b9355caa
Kritik Jain <kritikjain9@gmail.com>
2023-01-01 21:32:03 +0530

    Time: 62 ms (92.37%), Space: 41.7 MB (97.86%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,13 +2,9 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
-    let sum = 0;
-    
+var runningSum = function(nums) {    
     for(let i = 1; i < nums.length; i++){
-        // console.log(num);
         nums[i] = nums[i] + nums[i - 1];
     }
-    
     return nums;
 };
